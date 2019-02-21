3dd64cf00c6bb628722991991409d08e1720d3dc
Jan2 <Jan2@DESKTOP-8LAS7O1>
2019-02-21 21:02:33 +0100

    tapir, spock

--- src/lekce8/Game.java
+++ src/lekce8/Game.java
@@ -12,47 +12,54 @@ public class Game {
 
 	public void palyGame() {
 	
-	while(true) {
-	
-		System.out.println("\nZadejte \"kamen\", \"nuzky\", \"papir\" nebo pro ukonèení hry \"konec\": ");
-		String playerAnswer = getAnswerFromThePlayer();
-		String rndComputerAnswer = getRandomAnswerFromComputer();
-		String kamen = "kamen";
-		String nuzky = "nuzky";
-		String papir = "papir";
-		String konec = "konec";
+		while(true) {
+			String kamen = "kamen";
+			String nuzky = "nuzky";
+			String papir = "papir";
+			String tapir = "tapir";
+			String spock = "spock";		
+			String konec = "konec";
 		
-		if(playerAnswer.equals(kamen)|| playerAnswer.equals(nuzky) || playerAnswer.equals(papir)) {
-			if(playerAnswer.equals(rndComputerAnswer)) {
-				// remíza
-				System.out.println("Výsledek: Remíza. Oba hráèi hráli" + playerAnswer);
-			}
-			else if((playerAnswer.equals(kamen) && rndComputerAnswer.equals(nuzky))	|| 
-					(playerAnswer.equals(nuzky) && rndComputerAnswer.equals(papir)) || 
-					(playerAnswer.equals(papir) && rndComputerAnswer.equals(kamen)))	{
-				//player vyhrává
-				System.out.println("Výsledek: Hráè vítìzí. Hráè hrál" + playerAnswer + "Poèítaè hrál" + rndComputerAnswer);	
-			}
-			else {
-				System.out.println("Výsledek: Poèítaè vítìzí. Hráè hrál" + playerAnswer + "Poèítaè hrál" + rndComputerAnswer);
-			}
-		}
-		else if (playerAnswer.equals(konec)) {
-			break;
-		}
+			System.out.println("\nZadejte \"kamen\", \"nuzky\", \"papir\", \"tapir\", \"spock\" nebo pro ukonèení hry \"konec\": ");
+			String playerAnswer = getAnswerFromThePlayer();
+			String rndComputerAnswer = getRandomAnswerFromComputer();
 			
-		else {
-			System.out.println("Nevalidní odpovìï");	
-		}	
-	}
+			
+			if(playerAnswer.equals(kamen)|| playerAnswer.equals(nuzky) || playerAnswer.equals(papir)|| playerAnswer.equals(tapir) || playerAnswer.equals(spock)) {
+				if(playerAnswer.equals(rndComputerAnswer)) {
+					// remíza
+					System.out.println("Výsledek: Remíza. Oba hráèi hráli " + playerAnswer+ ".");
+				}
+				else if((playerAnswer.equals(kamen) && rndComputerAnswer.equals(nuzky))	|| 
+						(playerAnswer.equals(nuzky) && rndComputerAnswer.equals(papir)) || 
+						(playerAnswer.equals(papir) && rndComputerAnswer.equals(kamen)) ||
+						(playerAnswer.equals(tapir) && rndComputerAnswer.equals(spock)) ||
+						(playerAnswer.equals(spock) && rndComputerAnswer.equals(nuzky)) ||
+						(playerAnswer.equals(nuzky) && rndComputerAnswer.equals(tapir)) ||
+						(playerAnswer.equals(tapir) && rndComputerAnswer.equals(papir)) ||
+						(playerAnswer.equals(papir) && rndComputerAnswer.equals(spock)) ||
+						(playerAnswer.equals(spock) && rndComputerAnswer.equals(kamen))){
+					//player vyhrává
+					System.out.println("Výsledek: Hráè vítìzí. Hráè hrál " + playerAnswer + "." + " Poèítaè hrál " + rndComputerAnswer + ".");	
+				}
+				else {
+					System.out.println("Výsledek: Poèítaè vítìzí. Hráè hrál " + playerAnswer + "." + " Poèítaè hrál " + rndComputerAnswer + ".");
+				}
+			}
+			else if (playerAnswer.equals(konec)) {
+				break;
+			}
+				
+			else {
+				System.out.println("Nevalidní odpovìï");	
+			}	
+		}
 	}
 	
 	private String getAnswerFromThePlayer() {
-		
 		scan = new Scanner(System.in);
 		String answer = scan.nextLine();
 		return answer;
-	
 	}
 	
 	private String getRandomAnswerFromComputer() {
